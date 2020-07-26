b1783252994e0c27467de8f4b804759c931e56c4
Yudi Yamane <yudiyamane@gmail.com>
2020-07-25 23:11:45 -0300

    added fake page

---
+++ src/pages/UpcomingMovies/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const UpcomingMovies: React.FC = () => {
+  return (
+    <ul>
+      <li>Filme 1</li>
+      <li>Filme 2</li>
+    </ul>
+  );
+};
+
+export default UpcomingMovies;
