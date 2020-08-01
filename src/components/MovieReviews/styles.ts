aa5ed7c68ae8d7a87dc4dd069e8c7724ac9ab07f
Yudi Yamane <yudiyamane@gmail.com>
2020-08-01 19:26:03 -0300

    added box-shadow

--- src/components/MovieReviews/styles.ts
+++ src/components/MovieReviews/styles.ts
@@ -16,11 +16,22 @@ export const Review = styled.li`
   margin-bottom: 30px;
   padding: 18px;
   border-radius: 8px;
-  border: 1px solid #ddd;
+  border: 1px solid #eee;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.08);
+
+  transition: box-shadow 0.2s;
+
+  &:hover {
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.15);
+  }
 
   strong {
     text-transform: uppercase;
     display: block;
     margin-bottom: 16px;
+  }
+
+  div {
+    line-height: 22px;
   }
 `;
