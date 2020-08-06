62346cbb777324ceb4049131017c81e1f8b52d22
Yudi Yamane <yudiyamane@gmail.com>
2020-08-06 20:54:20 -0300

    simple header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,38 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, SearchBar, UserInfo } from './styles';
+
+interface User {
+  name: string;
+}
 
 const Header: React.FC = () => {
+  const authenticated = false;
+  const user = {
+    name: 'Yudi Yamane',
+  } as User;
+
   return (
     <Container>
-      <h1>hey</h1>
+      <h1>
+        <a href="/">MOVIEHUB</a>
+      </h1>
+
+      <SearchBar>
+        <input type="text" placeholder="Procure por filmes..." />
+      </SearchBar>
+
+      {/* não fica alinhado na página principal */}
+      <UserInfo>
+        {authenticated ? (
+          <>
+            <strong> {user.name} </strong>
+            <button type="button">Sair</button>
+          </>
+        ) : (
+          <button type="button">Entrar</button>
+        )}
+      </UserInfo>
     </Container>
   );
 };
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,3 +1,56 @@
 import styled from 'styled-components';
+import { lighten } from 'polished';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  padding: 30px 50px;
+
+  background: #171717;
+  color: #fff;
+
+  /* qual dos dois? */
+  /* height: 64px; */
+  height: 10vh;
+
+  nav ul {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+`;
+
+export const SearchBar = styled.div`
+  input {
+    width: 320px;
+    height: 36px;
+    border-radius: 10px;
+    border: 0;
+    padding: 15px;
+
+    ::placeholder {
+      color: #fff5;
+    }
+
+    color: #fff8;
+    background: ${lighten(0.1, '#171717')};
+  }
+`;
+
+export const UserInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+
+  button {
+    border: 0;
+    background: transparent;
+    color: #fff;
+  }
+
+  strong {
+    display: block;
+  }
+`;
